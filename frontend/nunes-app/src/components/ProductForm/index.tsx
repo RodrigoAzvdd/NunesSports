280c9097a808e104dc8edcf27c3f513672d7be0f
rodrigo azevedo silva <rodrigo.azvd750@gmail.com>
2024-01-05 16:37:31 -0300

    update price type on products table

--- frontend/nunes-app/src/components/ProductForm/index.tsx
+++ frontend/nunes-app/src/components/ProductForm/index.tsx
@@ -21,45 +21,39 @@ export default function ProductForm({ name, description, price, id, type }: {
 
     const createProduct = async () => {
         try {
-            if (!nameInput || !descriptionInput || !priceInput) {
-                console.log('Erro');
-            } else {
-                const newProduct = {
-                    name: nameInput.current?.value,
-                    description: descriptionInput.current?.value,
-                    price: Number(priceInput.current?.value)
-                }
-                await axios.post(`http://localhost:3300/products/`, newProduct)
-                setToastIsOpen(true)
-                setTimeout(() => {
-                    setToastIsOpen(false)
-                }, 3000);
-            }
+            const newProduct = {
+                name: nameInput.current?.value,
+                description: descriptionInput.current?.value,
+                price: Number(priceInput.current?.value)
+            };
+
+            await axios.post(`http://localhost:3300/products/`, newProduct)
+
+            setToastIsOpen(true)
+            setTimeout(() => {
+                setToastIsOpen(false)
+            }, 3000)
         } catch (error) {
-            console.log(error);
+            console.error("Erro ao cadastrar produto:", error)
         }
     }
 
     const updateProduct = async () => {
         try {
-            if (!nameInput || !descriptionInput || !priceInput) {
-                console.log('Erro');
-            } else {
-                const updatedProduct = {
-                    name: nameInput.current?.value,
-                    description: descriptionInput.current?.value,
-                    price: Number(priceInput.current?.value)
-                }
-                await axios.put(`http://localhost:3300/products/${id}`, updatedProduct);
-                setToastIsOpen(true)
-                setTimeout(() => {
-                    setToastIsOpen(false)
-                }, 3000);
-            }
-        } catch (error) {
-            console.log(error);
-            console.log(id);
+            const updatedProduct = {
+                name: nameInput.current?.value,
+                description: descriptionInput.current?.value,
+                price: Number(priceInput.current?.value)
+            };
 
+            await axios.put(`http://localhost:3300/products/${id}`, updatedProduct)
+
+            setToastIsOpen(true)
+            setTimeout(() => {
+                setToastIsOpen(false)
+            }, 3000)
+        } catch (error) {
+            console.error("Erro ao atualizar produto:", error)
         }
     }
 
@@ -69,16 +63,43 @@ export default function ProductForm({ name, description, price, id, type }: {
             <form className={styles.form}>
                 <div className={styles.inputDiv}>
                     <label className={styles.label} htmlFor="name">Nome:</label>
-                    <input ref={nameInput} className={styles.input} autoComplete='off' type="text" name="name" id="name" defaultValue={name ? name : ''} required />
+                    <input
+                        ref={nameInput}
+                        className={styles.input}
+                        autoComplete='off'
+                        type="text"
+                        name="name"
+                        id="name"
+                        defaultValue={name ? name : ''}
+                        required
+                    />
                 </div>
                 <div className={styles.inputDiv}>
                     <label className={styles.label} htmlFor="name">Descrição:</label>
-                    <input ref={descriptionInput} className={styles.input} autoComplete='off' type="text" name="name" id="name" defaultValue={description ? description : ''} required />
+                    <input
+                        ref={descriptionInput}
+                        className={styles.input}
+                        autoComplete='off'
+                        type="text"
+                        name="name"
+                        id="name"
+                        defaultValue={description ? description : ''}
+                        required
+                    />
                 </div>
                 <div className={styles.inputDiv}>
                     <label className={styles.label} htmlFor="name">Preço:</label>
-                    <input ref={priceInput} className={styles.input} autoComplete='off' type="number" name="name" id="name" min={1} defaultValue={price ? price.toFixed(2) : '1'} required />
-                </div>
+                    <input
+                        ref={priceInput}
+                        className={styles.input}
+                        autoComplete='off'
+                        type="number"
+                        name="name"
+                        id="name"
+                        step="0.01"
+                        defaultValue={price ? price.toFixed(2) : '1.00'}
+                        required
+                    />                </div>
                 {
                     type == "CREATE" ?
                         <button type='button' onClick={createProduct} className={styles.btn}>Cadastrar</button> :
